7aef3ec4d3b027131291334f7d0cbbad7e44ed94
zalachenka <saliou.armony@gmail.com>
2019-08-21 12:55:57 +0200

    first commit

---
+++ userQueries.js
@@ -0,0 +1,6 @@
+//Asking the user his first name, last name and age with prompt functions,
+// then displaying it with console.log
+const firstName = prompt("What is your first name ?");
+const lastName = prompt("What is your surname ?");
+const age = prompt("How old are you ?");
+console.log("Hello "+firstName+" "+lastName+", you are "+age+" years old.");
